71ca164dc7b74a9cf5c771707f5c5acaf3ff9827
Juariya Akter <juariyaakter97@gmail.com>
2024-02-20 14:34:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let shirtPrice = 1200;
+let t_shirtPrice = 900;
+
+if(t_shirtPrice <= shirtPrice){
+    console.log('shirtPrice');
+}else if(t_shirtPrice >= shirtPrice){
+    console.log('t_shirtPrice')
+}else{
+    console.log('shirtPrice')
+}
+
+
+var CarColor = 'red';
+var PrivetcarColor = 'pink';
+
+if(CarColor == PrivetcarColor){
+    console.log('car color')
+}else{
+    console.log(`privetcar coler`)
+}
